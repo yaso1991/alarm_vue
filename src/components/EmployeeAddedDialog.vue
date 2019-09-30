4e8ed92b72b2b77834e2e370387d4d4037f11bb0
Yaso <1721662545@qq.com>
2019-09-30 15:18:22 +0800

    dev v1.0 1. 添加elementUI.          √ 2. 添加登录功能.            √ 3. 添加监控页面功能.         √ 4. 添加和完善汇总页面        √ 5. 修改组件名称,和程序对应    √ 6. 添加并完善EmployeeInfo 组件    -->
    备注:
    1. 完善全局搜索功能.

---
+++ src/components/EmployeeAddedDialog.vue
@@ -0,0 +1,112 @@
+<template>
+  <el-dialog
+    :before-close="handleClose"
+    :visible.sync="dVisible"
+    style="text-align: center"
+    title="添加员工"
+    width="600px">
+    <el-form
+      :model="employeeInfo"
+      :rules="rules"
+      label-position="right"
+      label-width="80px"
+      style="padding-left: 140px"
+      ref="addEmployeeInfoForm">
+      <el-row>
+        <el-col :span="6">
+          <div>
+            <el-form-item label="姓名:" prop="name">
+              <el-input placeholder="请输入员工姓名" prefix-icon="el-icon-edit" style="width: 200px" v-model="employeeInfo.name" />
+            </el-form-item>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">
+          <div>
+            <el-form-item label="工号:" prop="workId">
+              <el-input placeholder="请输入工号" prefix-icon="el-icon-edit" style="width: 200px" v-model="employeeInfo.workId" />
+            </el-form-item>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">
+          <div>
+            <el-form-item label="推送等级:" prop="pushLevel">
+              <el-select
+                placeholder="选择推送等级"
+                style="width: 200px"
+                v-model="employeeInfo.pushLevel">
+                <el-option
+                  :key="item"
+                  :label="item"
+                  :value="item"
+                  v-for="item in pushLevels">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="7">
+          <div>
+            <el-form-item label="电子邮箱:" prop="email">
+              <el-input placeholder="请输入电子邮箱" prefix-icon="el-icon-edit" style="width: 200px" v-model="employeeInfo.email" />
+            </el-form-item>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="10">
+          <span>
+          <el-button @click="dVisible = false">取 消</el-button>
+          <el-button @click="submitEmployeeInfo" type="primary">确 定</el-button>
+        </span>
+        </el-col>
+      </el-row>
+
+    </el-form>
+  </el-dialog>
+
+</template>
+
+<script>
+  export default {
+    name: 'EmployeeAddedDialog',
+    rules: [],
+    data () {
+      return {
+        dVisible: false,
+        employeeInfo: {
+          id: '',
+          name: '',
+          workId: '',
+          pushLevel: '',
+          email: ''
+        },
+        pushLevels:[
+          '员工级',
+          '班组长级',
+          '主任级',
+          '经理级'
+        ]
+      }
+    },
+    methods: {
+      handleClose () {
+        this.dVisible = false
+      },
+      submitEmployeeInfo () {
+      },
+      show () {
+        this.dVisible = true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/EmployeeInfo.vue
+++ src/components/EmployeeInfo.vue
@@ -1,13 +1,239 @@
 <template>
+  <div style="text-align: left">
+    <el-row :gutter="10" style="margin-bottom: 10px">
+      <el-col span="12">
+        <el-input
+          placeholder="请输入内容"
+          prefix-icon="el-icon-search"
+          size="small"
+          style="width: 240px"
+          v-model="searchKey">
+        </el-input>
+        <el-button @click=" searchShow = !searchShow" icon="el-icon-search" style="margin-left: 0px" size="small" type="primary">搜索</el-button>
+        <el-button @click="showDialog" icon="el-icon-plus" style="margin-left: 0px" plain="true" size="small" type="primary">添加
+        </el-button>
+        <el-button @click="" icon="el-icon-upload" style="margin-left: 0px" plain="true" size="small" type="primary">
+          修改
+        </el-button>
+        <el-button @click="" icon="el-icon-close" style="margin-left: 0px" plain="true" size="small" type="danger">删除
+        </el-button>
+        <span>
+          <el-upload
+            :before-remove="beforeRemove"
+            :file-list="fileList"
+            :limit="3"
+            :on-exceed="handleExceed"
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            class="upload-demo"
+            multiple
+            style="display: inline">
+            <el-button @click="" icon="el-icon-top" plain="true" size="small" type="primary">导出</el-button>
+          </el-upload>
+        </span>
+      </el-col>
+      <el-col span="12" style="text-align: right;">
+        <el-row style="text-align: right">
+          <el-pagination
+            :total="100"
+            background
+            layout="prev, pager, next">
+          </el-pagination>
+        </el-row>
+      </el-col>
+    </el-row>
+    <el-collapse-transition>
+      <div class="transition-box" style="margin: 10px 0px;border-radius: 3px;border: #E4E7ED solid 1px;padding-top: 20px;padding-left: 20px"
+           v-show="searchShow">
+        <el-row>
+          <el-form :inline="true" :model="searchForm" class="demo-form-inline">
+            <el-col :span="16">
+              <el-form-item label="姓名:">
+                <el-input v-model="searchForm.name" size="small" placeholder="请输入姓名"/>
+              </el-form-item>
+              <el-form-item label="工号:">
+                <el-input v-model="searchForm.workId" size="small" placeholder="请输入工号"/>
+              </el-form-item>
+              <el-form-item label="推送等级:">
+                <el-input placeholder="请输入推送等级" size="small" v-model="searchForm.pushLevel"/>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8" style="text-align:right">
+              <el-form-item>
+                <el-button @click="onSubmit" size="small" type="primary">查询</el-button>
+                <el-button @click="searchShow = false" size="small" type="danger">取消</el-button>
+              </el-form-item>
+            </el-col>
+          </el-form>
+        </el-row>
+      </div>
+    </el-collapse-transition>
+    <el-row>
+      <EmployeeAddedDialog ref="employeeAddedDialog" />
+    </el-row>
+    <el-row style="margin-bottom: 10px">
+      <el-table
+        :data="tableShowData"
+        @selection-change="handleSelectChange"
+        border
+        height="800"
+        max-height="800"
+        size="mini"
+        stripe
+        style="width: 100%">
+        <el-table-column
+          align="center"
+          type="selection"
+          width="50">
+        </el-table-column>
+        <el-table-column
+          align="center"
+          fixed
+          label="员工姓名"
+          prop="name"
+          width="400">
+        </el-table-column>
+        <el-table-column
+          align="center"
+          label="员工编号"
+          prop="workId"
+          width="400">
+        </el-table-column>
+        <el-table-column
+          align="center"
+          label="推送等级"
+          prop="pushLevel"
+          width="300">
+        </el-table-column>
+        <el-table-column
+          align="center"
+          label="电子邮件"
+          prop="email"
+          width="550">
+        </el-table-column>
+      </el-table>
+    </el-row>
 
+  </div>
 </template>
 
 <script>
+import EmployeeAddedDialog from './EmployeeAddedDialog'
+
   export default {
-    name: 'EmployeeInfo'
+    name: 'EmployeeInfo',
+    components:{
+      EmployeeAddedDialog
+    },
+    data () {
+      return {
+        searchForm: {
+          name:'',
+          workId:'',
+          pushLevel:''
+        },
+        alarmPoints: [
+          '1号线中轧测厚',
+          '2号线中轧测厚',
+          '3号线中轧测厚',
+          '4号线中轧测厚',
+          '5号线中轧测厚',
+          '6号线中轧测厚',
+        ],
+        searchShow: true,
+        searchKey: '',
+        tableShowData: [],
+        alarmItems: [
+          {
+            id: 1,
+            name: '班组长A',
+            workId: '555',
+            pushLevel: '班组长',
+            email: '1721662545@qq.com'
+          },
+          {
+            id: 1,
+            name: '班组长B',
+            workId: '555',
+            pushLevel: '班组长',
+            email: '1721662545@qq.com'
+          },
+          {
+            id: 1,
+            name: '员工A',
+            workId: '555',
+            pushLevel: '员工A',
+            email: '1721662545@qq.com'
+          },
+          {
+            id: 1,
+            name: '员工A',
+            workId: '555',
+            pushLevel: '员工A',
+            email: '1721662545@qq.com'
+          },
+          {
+            id: 1,
+            name: '员工A',
+            workId: '555',
+            pushLevel: '员工A',
+            email: '1721662545@qq.com'
+          },
+          {
+            id: 1,
+            name: '员工A',
+            workId: '555',
+            pushLevel: '员工A',
+            email: '1721662545@qq.com'
+          },
+          {
+            id: 1,
+            name: '员工A',
+            workId: '555',
+            pushLevel: '员工A',
+            email: '1721662545@qq.com'
+          },
+          {
+            id: 1,
+            name: '员工A',
+            workId: '555',
+            pushLevel: '员工A',
+            email: '1721662545@qq.com'
+          }
+
+        ],
+        selectedColums: []
+      }
+    },
+    methods: {
+      handleDelete () {
+      },
+      handleUpdate () {
+        if (this.selectedColums.length == 1) {
+          this.$refs.employeeUpdateDialog.show(this.selectedColums[0])
+        }
+      },
+      getInfosByPage (page, size = 20) {
+        this.tableShowData = this.alarmItems
+      },
+      handleSearchEmployee () {
+      },
+      handleSelectChange (selection) {
+        this.selectedColums = selection
+      },
+      showDialog () {
+        this.$refs.employeeAddedDialog.show()
+      }
+    },
+    mounted: function () {
+      this.getInfosByPage(1)
+    }
   }
+
 </script>
 
 <style scoped>
 
 </style>
+
